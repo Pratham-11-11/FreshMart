cb4d1a04b11e4bc75bf93eb26decf1c0f6f007d6
Pratham Jitendra Lingayat <prathamlingayat04@gmail.com>
2026-08-11 13:41:04 +0530

    Update API_URL to use production backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,9 @@ import React, { useEffect, useMemo, useState } from "react";
 import "./App.css";
 import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api";
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  "https://freshmart-backend-x3gy.onrender.com/";
 
 const categoryIcons = {
   fruits: "🍎",
